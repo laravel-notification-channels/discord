32d5714692c28982b8b785554173f5005ef40546
jee7 <raimondtunnel@hotmail.com>
2020-10-13 04:05:44 +0300

    Propagate the Discord API response exceptions (#42)

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -10,10 +10,12 @@ class CouldNotSendNotification extends Exception
 {
     /**
      * @param \Psr\Http\Message\ResponseInterface $response
+     * @param int $code
+     * @param \Exception $exception
      *
      * @return static
      */
-    public static function serviceRespondedWithAnHttpError(ResponseInterface $response)
+    public static function serviceRespondedWithAnHttpError(ResponseInterface $response, $code, $exception)
     {
         $message = "Discord responded with an HTTP error: {$response->getStatusCode()}";
 
@@ -21,17 +23,18 @@ class CouldNotSendNotification extends Exception
             $message .= ": $error";
         }
 
-        return new static($message);
+        return new static($message, $code, $exception);
     }
 
     /**
      * @param array $response
+     * @param int $code
      *
      * @return static
      */
-    public static function serviceRespondedWithAnApiError(array $response)
+    public static function serviceRespondedWithAnApiError(array $response, $code, $exception)
     {
-        return new static("Discord responded with an API error: {$response['code']}: {$response['message']}");
+        return new static("Discord responded with an API error: {$response['code']}: {$response['message']}", $code);
     }
 
     /**
@@ -41,6 +44,6 @@ class CouldNotSendNotification extends Exception
      */
     public static function serviceCommunicationError(Exception $exception)
     {
-        return new static("Communication with Discord failed: {$exception->getCode()}: {$exception->getMessage()}");
+        return new static("Communication with Discord failed: {$exception->getCode()}: {$exception->getMessage()}", $exception->getCode(), $exception);
     }
 }
